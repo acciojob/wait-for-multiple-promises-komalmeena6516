a59e50aa317554d92172e59b609e8d1171fff741
Komal Meena <80938016+komalmeena6516@users.noreply.github.com>
2023-04-13 17:54:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 //your JS code here. If required.
+const tBody = document.getElementById('output')
+
+const promises = [
+  new Promise((resolve, reject) => {
+    const time = Math.floor(Math.random()*3+1)*1000;
+    setTimeout(() => {
+      resolve({name:"Promise 1", time: time/1000})
+    }, time);
+  }),
+  new Promise((resolve, reject) => {
+    const time = Math.floor(Math.random()*3+1)*1000;
+    setTimeout(() => {
+      resolve({name:"Promise 2", time: time/1000})
+    }, time);
+  }),
+  new Promise((resolve, reject) => {
+    const time = Math.floor(Math.random()*3+1)*1000;
+    setTimeout(() => {
+      resolve({name:"Promise 3", time: time/1000})
+    }, time);
+  }),
+];
+
+
+async function Allfunc(){
+const start = new Date();
+
+tBody.innerHTML += `
+<tr id="loading">
+  <td colspan=2>Loading...</td>
+  </tr>
+  `;
+
+  await Promise.all(promises)
+  .then((result)=>{
+    tBody.innerHTML = ``;
+    //result is a output of all promises in array form
+    //use foreach to iterate and add result at table the 
+    result.forEach((e)=>{
+      tBody.innerHTML +=`
+      <tr>
+                <td>${e.name}</td>
+                <td>${e.time}</td>
+            </tr>
+      `;
+    });
+
+  }).catch((error)=>{
+    console.log(error);
+  });
+
+  const end = new Date();
+  const milliTime = end-start;
+  tBody.innerHTML += `
+  <tr>
+                <td>Total</td>
+                <td>${milliTime/ 1000}</td>
+            </tr>
+  `;
+}
+
+Allfunc();
